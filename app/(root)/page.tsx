066dc992600e7247811776e9a6a0a9cbb66cd847
Abraham <abraham.sulit@yahoo.com>
2026-03-12 16:59:24 +0800

    Implement Local Search Bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,40 @@
-import { auth } from "@/auth";
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/route";
+import Link from "next/link";
+const questions = [
+  { _id: "1", title: "How to learn React" },
+  { _id: "2", title: "How to learn Javascript" },
+];
 
-const Home = async () => {
-  const session = await auth();
-  console.log(session)
+interface SearchParams {
+  searchParams : Promise<{[key: string]:string}>
+}
+const Home = async ({searchParams}:SearchParams) => {
+  const {query = ""} = await searchParams
+  const filteredQuestions = questions.filter((question)=>question.title.toLowerCase().includes(query?.toLowerCase()))
   return (
     <>
-      <h1 className="h1-bold font-inter">Welcome to the world of NextJs</h1>
+      <section className="flex w-full flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      {/* HomeFilter */}
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
+      </div>
     </>
   );
 };
